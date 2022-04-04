7ec2acc55972eb80fe943b313e9d4f0decbda171
Richard <51135299+rzwc@users.noreply.github.com>
2022-04-03 20:57:21 -0600

    Added header navigation and routing

--- ucapital/src/App.js
+++ ucapital/src/App.js
@@ -1,15 +1,43 @@
 import News from "./pages/news"
 import './App.css'
+import { BrowserRouter, Route, Routes, Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div class="header">
 
-      </div>
-      <News></News>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route path="news" element={<News />} />
+          </Route>
+        </Routes>
+        {/* <div class="header">
+
+        </div>
+        <News></News> */}
+
+      </BrowserRouter>
+
+    </div >
   );
+}
+
+function Header() {
+  return (
+    <div>
+      <div class="header">
+        {/* <Link to="/news">News</Link> */}
+        {/* <Link to='news'></Link> */}
+        <nav>
+          <Link to='news'>News</Link>
+        </nav>
+      </div>
+      <Outlet />
+    </div>
+
+
+  )
 }
 
 export default App;
