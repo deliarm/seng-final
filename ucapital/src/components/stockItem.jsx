70caa992fd8567fd1859edab1480f857425740f8
Richard <51135299+rzwc@users.noreply.github.com>
2022-04-09 17:06:56 -0600

    css changes

--- ucapital/src/components/stockItem.jsx
+++ ucapital/src/components/stockItem.jsx
@@ -3,24 +3,24 @@ import './stockitem.css'
 import Plot from 'react-plotly.js';
 // var yfinance = require('yfinance');
 
-class StockItem extends React.Component{
-  constructor(props){
+class StockItem extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       stockXValues: [],
       stockYValues: [],
-      active : false,
+      active: false,
     }
     this.toggle = this.toggle.bind(this);
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.fetchStock();
   }
-  
-  fetchStock(){
+
+  fetchStock() {
     const pointer = this;
-    var KEY =  "5A8Z4LIZ2FPZ4RCF";
+    var KEY = "5A8Z4LIZ2FPZ4RCF";
     var stock = this.props.stockName;
     var API_call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&interval=5min&outputsize=full&apikey=${KEY}`;
     let stockXValuesFunction = [];
@@ -28,44 +28,44 @@ class StockItem extends React.Component{
 
     fetch(API_call)
       .then(
-        function(response){
+        function (response) {
           return response.json();
         }
       )
       .then(
-        function(data){
+        function (data) {
           // console.log(data);
-          for (var key in data['Time Series (Daily)']){
+          for (var key in data['Time Series (Daily)']) {
             stockXValuesFunction.push(key); // date
             stockYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']); // price
           }
           pointer.setState({
-            stockXValues : stockXValuesFunction,
-            stockYValues : stockYValuesFunction,
-            stockSymbol : stock,
-            curr_price : stockYValuesFunction[0],
+            stockXValues: stockXValuesFunction,
+            stockYValues: stockYValuesFunction,
+            stockSymbol: stock,
+            curr_price: stockYValuesFunction[0],
           });
         }
       )
   }
 
-  toggle(){
-    if(this.state.active === false){
+  toggle() {
+    if (this.state.active === false) {
       this.setState({
-        active : true
+        active: true
       });
       console.log('set')
     }
-    else{
+    else {
       this.setState({
-        active : false
+        active: false
       });
       console.log('unset')
     }
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div id="stockContainer" className={'stockContainer' + (this.state.active ? "--active" : "")} onDoubleClick={this.toggle}>
         <div id="textContainer">
           <h1>{this.state.stockSymbol}</h1>
@@ -73,45 +73,45 @@ class StockItem extends React.Component{
         </div>
 
         <Plot id={this.state.StockSymbol}
-        data={[
-          {
-            x: this.state.stockXValues,
-            y: this.state.stockYValues,
-            type: 'scatter',
-            mode: 'lines',
-            marker: {color: '#00fa9a'}, // 6FD08C
-            hoverinfo: "x+y",
-            fill:'tozeroy',
-            opacity: ".1",
-          },
-        ]}
-        layout={ { 
-          // title: this.state.stockSymbol,
-          // width: 480,
-          height: '100%',
-          useResizeHandler : true,
-          autosize: true,
-          plot_bgcolor: "hsl(216, 50%, 16%)",
-          paper_bgcolor: "hsl(216, 50%, 16%)",
-          font: {
-            color: "#fff",
-          },
-          margin:{
-            // autoexpand: true,
-            b : 40,
-            t : 10,
-            l : 40,
-            r : 10,
+          data={[
+            {
+              x: this.state.stockXValues,
+              y: this.state.stockYValues,
+              type: 'scatter',
+              mode: 'lines',
+              marker: { color: '#00fa9a' }, // 6FD08C
+              hoverinfo: "x+y",
+              fill: 'tozeroy',
+              opacity: ".1",
+            },
+          ]}
+          layout={{
+            // title: this.state.stockSymbol,
+            // width: 480,
+            // height: '100%',
+            // useResizeHandler: true,
+            autosize: true,
+            plot_bgcolor: "hsl(216, 50%, 16%)",
+            paper_bgcolor: "hsl(216, 50%, 16%)",
+            font: {
+              color: "#fff",
+            },
+            margin: {
+              // autoexpand: true,
+              b: 40,
+              t: 10,
+              l: 40,
+              r: 10,
+            }
           }
-        } 
-        }
-        config={{
-        //responsive : true // causes improper first load -> need resolution
-        }}
-        style={{
-          width:"100%",
-          height:"100%",
-        }}
+          }
+          config={{
+            //responsive : true // causes improper first load -> need resolution
+          }}
+          style={{
+            width: "100% !important",
+            height: "800px",
+          }}
         />
 
       </div>
--- ucapital/src/pages/stock.jsx
+++ ucapital/src/pages/stock.jsx
@@ -6,15 +6,15 @@ import globalStocks from "../components/globalStocks";
 const stock = () => {
     function shuffle(array) {
         array.sort(() => Math.random() - 0.5);
-      }      
+    }
     const stock_array = JSON.parse(globalStocks.JsonStocks);
-    const new_array = (stock_array.sort((a, b) => 0.5 - Math.random())).slice(0,1);
+    const new_array = (stock_array.sort((a, b) => 0.5 - Math.random())).slice(0, 5);
 
-    return(
+    return (
         <div id="pageContainer">
-            <h1 id="title"> Today's Stocks </h1>            
+            <h1 id="title"> Today's Stocks </h1>
             <ul id="mainContent">
-                {new_array.map(function(s){
+                {new_array.map(function (s) {
                     return <li id="item" key={s}><StockItem stockName={s}></StockItem></li>;
                 })}
             </ul>
