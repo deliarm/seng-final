9d9adc298e842e2ee21e6c2ea5fd2f72750a3164
Richard <51135299+rzwc@users.noreply.github.com>
2022-04-10 12:53:05 -0600

    change back to default

--- ucapital/src/App.js
+++ ucapital/src/App.js
@@ -3,55 +3,50 @@ import Stock from "./pages/stock";
 import StockItem from "./components/stockItem";
 import Login from './pages/loginpage'
 import Signup from './pages/signuppage'
-import Home from './pages/home'
 import './App.css'
-import { BrowserRouter, Route, Routes, Link, Outlet, useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link, Outlet } from "react-router-dom";
 import { useState } from 'react'
 
 function App() {
   const [user, setLoginUser] = useState({})
   const bool = true
-  const news = false
-  const home = true
-  // if(!(user && user._id)){
-  //   return(
-  //     <div className="App">
-  //       <BrowserRouter>
-  //         <Routes>
-  //           <Route path='/' element={<Login setLoginUser={setLoginUser}/>} />
-  //           <Route path='/signup' element={<Signup />} />
-  //         </Routes>
-  //       </BrowserRouter>
+  if (!(user && user._id)) {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Login setLoginUser={setLoginUser} />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
 
-  //     </div>
-  //   )
-  // }
-  // else{
-  return (
-    <div className="App">
-      {/* <StockItem stockName="AAPL"></StockItem> */}
-      {/* <Stock></Stock> */}
-      <BrowserRouter>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="App">
+        {/* <StockItem stockName="AAPL"></StockItem> */}
+        <Stock></Stock>
+        {/* <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header setLoginUser={setLoginUser} />}>
+          <Route path="/"  element={<Header />}>
             <Route path="news" element={<News />} />
-            <Route path="home" element={<Home />} />
-          </Route>
-        </Routes>
-        <div class="header">
+          </Route> */}
+        {/* </Routes> */}
+        {/* <div class="header">
 
         </div>
-        <Home></Home>
+        <News></News> */}
 
-      </BrowserRouter>
+        {/* </BrowserRouter> */}
 
-    </div >
-  );
-  //}
+      </div >
+    );
+  }
 }
 
-function Header({ setLoginUser }) {
-  const navigate = useNavigate()
+function Header() {
   return (
     <div>
       <div className="header">
@@ -59,8 +54,6 @@ function Header({ setLoginUser }) {
         {/* <Link to='news'></Link> */}
         <nav>
           <Link to='news' className="linkStyle">News</Link>
-          <Link to='home' className="linkStyle">Home</Link>
-          <button className="logout-style" onClick={() => { setLoginUser({}); navigate("/") }}>Logout</button>
         </nav>
       </div>
       <Outlet />
