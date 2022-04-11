486412e9c12898af4f46bbda7174c99fd2635314
DeliarM <lioneldeliar520@gmail.com>
2022-04-11 00:17:05 -0600

    sorting/searching/loading for stock & crypto page done

--- ucapital/src/pages/crypto.jsx
+++ ucapital/src/pages/crypto.jsx
@@ -1,22 +1,70 @@
-import React from "react";
+import React, {useState} from 'react';
 import CryptoItem from "../components/cryptoItem";
 import './crypto.css'
 import globalCrypto from "../components/globalCrypto";
+const crypto_array = JSON.parse(globalCrypto.JsonCrypto);
+const new_array = crypto_array.slice(0, 25);
 
-const crypto = () => {
-    const crypto_array = JSON.parse(globalCrypto.JsonCrypto).slice(0,30);
-    // const new_array = (crypto_array.sort((a, b) => 0.5 - Math.random())).slice(0, 50);
+const Crypto = () => {
+    const [list,setList] = useState(new_array);
+    const [count,setCount] = useState(26);
+
+    function Search(){
+        var searched = document.getElementById("search").value.toUpperCase();
+        if(searched === ""){
+            setList(crypto_array.slice(0,count));
+            checkSort();
+        }
+        if(searched){
+            setList([])
+            crypto_array.forEach(element => {
+                if(element.includes(searched)){
+                    setList(prevList => [...prevList, element])
+                }
+            });
+            document.getElementById("search").value="";
+            document.getElementById("dropDown").selectedIndex = 0;
+        }
+    }
+
+    function checkSort(){
+        var sortMethod = document.getElementById("dropDown");
+        var selected = sortMethod.options[sortMethod.selectedIndex].innerHTML;
+        if(selected === "Alphabetical A-Z"){
+            var temp = list.sort();
+            setList([...temp])
+        }
+        else{
+            var reverse = (list.sort()).reverse()
+            setList([...reverse])
+        }
+    }
+
+    function loadCrypto(){
+        var addon = crypto_array.slice(count,count+12);
+        setList(prevList => [...prevList,...addon]);
+        setCount(count+12);
+    }
 
     return (
         <div id="pageContainer">
-            <h1 id="title"> Today's Crypto </h1>
+            <h1 id="title"> Trending Cryptocurrencies </h1>
+            <div id="menu">
+                <select id="dropDown">
+                    <option value="alphabetical" >Alphabetical A-Z</option>
+                    <option value="reverse">Reverse Alphabetical Z-A</option>
+                </select>
+                <input id="search" type="text" placeholder="Search"/>
+                <button onClick={() => Search()}>Enter</button>
+            </div>
             <ul id="mainContent">
-                {crypto_array.map(function (s) {
+                {list.map(function (s) {
                     return <li id="item" key={s}><CryptoItem cryptoName={s}></CryptoItem></li>;
                 })}
             </ul>
+            <button onClick={loadCrypto}> load more CryptoCurrencies</button>
         </div>
     )
 }
 
-export default crypto
+export default Crypto
--- ucapital/src/pages/stock.jsx
+++ ucapital/src/pages/stock.jsx
@@ -1,25 +1,70 @@
-import React from "react";
+import React , {useState} from "react";
 import StockItem from "../components/stockItem";
 import './stock.css'
 import globalStocks from "../components/globalStocks";
+const stock_array = JSON.parse(globalStocks.JsonStocks);
+const new_array = (stock_array).slice(0, 26);
 
-const stock = () => {
-    function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
+const Stock = () => {
+    const [list,setList] = useState(new_array);
+    const [count,setCount] = useState(26);
+ 
+    function Search(){
+        var searched = document.getElementById("search").value.toUpperCase();
+        if(searched === ""){
+            setList(stock_array.slice(0,count));
+            checkSort();
+        }
+        if(searched){
+            setList([])
+            stock_array.forEach(element => {
+                if(element.includes(searched)){
+                    setList(prevList => [...prevList, element])
+                }
+            });
+            document.getElementById("search").value="";
+            document.getElementById("dropDown").selectedIndex = 0;
+        }        
     }
-    const stock_array = JSON.parse(globalStocks.JsonStocks);
-    const new_array = (stock_array.sort((a, b) => 0.5 - Math.random())).slice(0, 30);
+
+    function checkSort(){
+        var sortMethod = document.getElementById("dropDown");
+        var selected = sortMethod.options[sortMethod.selectedIndex].innerHTML;
+        if(selected === "Alphabetical A-Z"){
+            var temp = list.sort();
+            setList([...temp])
+        }
+        else{
+            var reverse = (list.sort()).reverse()
+            setList([...reverse])
+        }
+    }
+
+    function loadStocks(){
+        var addon = stock_array.slice(count,count+12);
+        setList(prevList => [...prevList,...addon]);
+        setCount(count+12);
+    }
 
     return (
         <div id="pageContainer">
             <h1 id="title"> Today's Stocks </h1>
+            <div id="menu">
+                <select id="dropDown">
+                    <option value="alphabetical" >Alphabetical A-Z</option>
+                    <option value="reverse">Reverse Alphabetical Z-A</option>
+                </select>
+                <input id="search" type="text" placeholder="Search"/>
+                <button onClick={() => Search()}>Enter</button>
+            </div>
             <ul id="mainContent">
-                {new_array.map(function (s) {
+                {list.map(function (s) {
                     return <li id="item" key={s}><StockItem stockName={s}></StockItem></li>;
                 })}
             </ul>
+            <button onClick={loadStocks}> load more Stocks</button>
         </div>
     )
 }
 
-export default stock
+export default Stock
