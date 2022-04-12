da7e9dafb2ae13f737b4950d3f5c6728c6162cf9
mojanjua1 <muhammad.janjua@ucalgary.ca>
2022-04-12 16:55:10 -0600

    home split into two

--- ucapital/src/pages/home.js
+++ ucapital/src/pages/home.js
@@ -1,12 +1,18 @@
 import ChatAppClient from "../components/chatApp/client/src/App"
+import "./home.css"
 
 import React from 'react'
 
 function home({user}) {
   return (
     <div className="homepage">
+      <div className="right">
       {/* Chat application was taken and modified from: https://github.com/machadop1407/react-socketio-chat-app */}
       <ChatAppClient user = {user}/>
+      </div>
+      <div className="left">
+        <h1>Favorites</h1>
+      </div>
     </div>
   )
 }
